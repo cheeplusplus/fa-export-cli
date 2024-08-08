2ec8c688cd0fd8bc70f9f2b677cb0eb4c1a67843
Kauko <kauko@biosynth.link>
2024-08-07 21:01:29 -0700

    Fix nix shell builds

--- build-macos.sh
+++ build-macos.sh
@@ -3,6 +3,7 @@
 npm install
 npm install --no-save postject
 npm run build
+node --experimental-sea-config sea-config.json
 cp $(command -v node) build/fa-export-cli
 codesign --remove-signature build/fa-export-cli 
 npx postject build/fa-export-cli NODE_SEA_BLOB build/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 --macho-segment-name NODE_SEA
