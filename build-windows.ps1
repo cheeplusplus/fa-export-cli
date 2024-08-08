bece973207f58b1ac9faedc193a599597fd79270
Kauko <kauko@biosynth.link>
2024-08-07 21:23:15 -0700

    Don't resign for now

--- build-windows.ps1
+++ build-windows.ps1
@@ -5,4 +5,4 @@ node --experimental-sea-config sea-config.json
 node -e "require('fs').copyFileSync(process.execPath, 'build/fa-export-cli.exe')" 
 C:\"Program Files (x86)\Windows Kits\10\App Certification Kit"\signtool.exe remove /s build\fa-export-cli.exe
 npx postject build/fa-export-cli.exe NODE_SEA_BLOB build/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
-C:\"Program Files (x86)\Windows Kits\10\App Certification Kit"\signtool.exe sign /fd SHA256 build\fa-export-cli.exe
+# C:\"Program Files (x86)\Windows Kits\10\App Certification Kit"\signtool.exe sign /fd SHA256 build\fa-export-cli.exe
